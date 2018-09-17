e322859cbf9b0e3a27b0b9e68ed66831e1703fee
6juara9 <chyzhykvv@gmail.com>
2018-09-17 22:37:13 +0300

    update

---
+++ XTestAvalonia.UI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using XTestAvalonia.UI.ViewModels;
+using XTestAvalonia.UI.Views;
+
+namespace XTestAvalonia.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
