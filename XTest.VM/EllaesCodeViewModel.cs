f93950a2c1b7760517a9772a83474beb39b72d5c
6juara9 <chyzhykvv@gmail.com>
2018-09-20 21:09:34 +0300

    viewModel

---
+++ XTest.VM/EllaesCodeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using XTest.Services.Entities;
+using XTest.Services.Services;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace XTest.VM
+{
+    public class EllaesCodeViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<EllaesCode> EllaesCodes { get; set; }
+        public EllaesCodeViewModel()
+        {
+            EllaesCodeService service = new EllaesCodeService();
+            EllaesCodes = new ObservableCollection<EllaesCode>
+            {
+                new EllaesCode { Array = service.GenerateArray(5,5) },
+                new EllaesCode { Array = service.GenerateArrayWithException(5,5) }
+            };
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
