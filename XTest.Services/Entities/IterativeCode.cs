1142ec2677ddf2eaf4ee321295d5cb05f6b4574d
Maxim Kazarin <kazzarin99@gmail.com>
2018-09-20 21:38:50 +0300

    View added

---
+++ XTest.Services/Entities/IterativeCode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XTest.Services.Entities
+{
+    class IterativeCode
+    {
+        public int[][] Array { get; set; }
+    }
+}
---
+++ XTest.VM/IterativeCodeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using XTest.Services.Entities;
+using XTest.Services.Services;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace XTest.VM
+{
+    class IterativeCodeViewModel
+    {
+    }
+}
